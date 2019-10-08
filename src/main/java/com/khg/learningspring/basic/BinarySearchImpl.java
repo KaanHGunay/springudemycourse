ef3ac41fbb90a0fc72117f993ac580964fa4be53
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-08 16:51:17 +0300

    Lifecycle of a Bean - PostConstruct and PreDestroy

--- src/main/java/com/khg/learningspring/basic/BinarySearchImpl.java
+++ src/main/java/com/khg/learningspring/basic/BinarySearchImpl.java
@@ -1,16 +1,22 @@
 package com.khg.learningspring.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Arrays;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)  // Her çağırıldığında yeni instance oluşturulursun ( Def. Singleton - App. Context için 1 tane )
 public class BinarySearchImpl {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("BubbleSort")  // Birden fazla implementasyon varsa isimlendirme çağırılabilir.
@@ -20,5 +26,15 @@ public class BinarySearchImpl {
         int[] sortedNumbers = sortAlgorithm.sort(numbers);
         System.out.println(Arrays.toString(sortedNumbers));
         return 3;
+    }
+
+    @PostConstruct  // Class oluştuktan sonra
+    public void postConstrucy() {
+        LOGGER.info("Post Construct Method Triggered");
+    }
+
+    @PreDestroy  // Class yok edilmeden önce
+    public void preDestroy() {
+        LOGGER.info("Pre destroy method triggered.");
     }
 }
