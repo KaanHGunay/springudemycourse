9a65772962c391266de8d77b3273ec7e9b6f301c
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-08 13:02:18 +0300

    Bean Life Cycle 1 - Singleton (Default) Her Application Context için bir instance 2 - Prototype Her çağırılmada yeni instance

--- src/main/java/com/khg/learningspring/BinarySearchImpl.java
+++ src/main/java/com/khg/learningspring/BinarySearchImpl.java
@@ -2,11 +2,14 @@ package com.khg.learningspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)  // Her çağırıldığında yeni instance oluşturulursun ( Def. Singleton - App. Context için 1 tane )
 public class BinarySearchImpl {
 
     @Autowired
--- src/main/java/com/khg/learningspring/LearningspringApplication.java
+++ src/main/java/com/khg/learningspring/LearningspringApplication.java
@@ -10,6 +10,8 @@ public class LearningspringApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(LearningspringApplication.class, args);
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+		// Scope Prototype olması nedeniyle iki farklı instance oluşturulacak
 		int a = binarySearch.binarySearch(new int[] {2, 6, 5}, 4);
 	}
 
