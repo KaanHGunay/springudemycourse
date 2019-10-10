21ecbe1de49d3e704b92ddeba2a940ec5b123a9a
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-10 18:09:24 +0300

    Spring Boot Section

---
+++ spring-boot-udemy-course/src/main/java/com/khg/springbootudemycourse/SpringBootUdemyCourseApplication.java
@@ -0,0 +1,13 @@
+package com.khg.springbootudemycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUdemyCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUdemyCourseApplication.class, args);
+	}
+
+}
--- spring-boot-udemy-course/src/test/java/com/khg/springbootudemycourse/SpringBootUdemyCourseApplicationTests.java
+++ spring-boot-udemy-course/src/test/java/com/khg/springbootudemycourse/SpringBootUdemyCourseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.khg.learningspring;
+package com.khg.springbootudemycourse;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
-public class LearningspringBasicApplicationTests {
+@SpringBootTest
+public class SpringBootUdemyCourseApplicationTests {
 
 	@Test
 	public void contextLoads() {
