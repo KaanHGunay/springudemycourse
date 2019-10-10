6d319a193ea74acd353e1a50635d323f3c7f4cef
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-10 14:15:47 +0300

    JUnit using of before and after

--- src/test/java/com/khg/junit/myMathTest.java
+++ src/test/java/com/khg/junit/myMathTest.java
@@ -1,11 +1,32 @@
 package com.khg.junit;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class myMathTest {
     myMath myMath = new myMath();
+
+    @Before  // Her Test ten önce çalıştırılacacak kodlar
+    public void before() {
+        System.out.println("Before method triggered.");
+    }
+
+    @After  // Her Test ten sonra çalıştırılacacak kodlar
+    public void after() {
+        System.out.println("After method triggered.");
+    }
+
+    @BeforeClass  // Tüm tessten önce çalışacak kodlar
+    public static void beforeClass() {
+        System.out.println("Before Class triggered.");
+    }
+
+    @AfterClass  // Tüm tessten sonra çalışacak kodlar
+    public static void afterClass() {
+        System.out.println("After Class triggered.");
+    }
+
     @Test
     public void sum_with_1_numbers() {
         assertEquals(1, myMath.sum(new int[] {1}));
