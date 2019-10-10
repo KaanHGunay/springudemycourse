0c046c6c80e91ec6c5662e33e8ede1ebe6500195
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-10 10:59:02 +0300

    Mixing XML and Annotation

--- src/main/java/com/khg/learningspring/LearningspringXmlApplication.java
+++ src/main/java/com/khg/learningspring/LearningspringXmlApplication.java
@@ -1,15 +1,19 @@
 package com.khg.learningspring;
 
 import com.khg.learningspring.xml.XmlPersonDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LearningspringXmlApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(LearningspringXmlApplication.class);
 
 	public static void main(String[] args) {
 		try(ClassPathXmlApplicationContext applicationContext =
 					new ClassPathXmlApplicationContext("applicationContext.xml")) {
+			LOGGER.info("Beans Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
 			XmlPersonDao xmlPersonDao = applicationContext.getBean(XmlPersonDao.class);
-			System.out.println(xmlPersonDao.getXmlJdbcConnection());
+			LOGGER.info("{} {}", xmlPersonDao, xmlPersonDao.getXmlJdbcConnection());
 		}
 	}
 }
