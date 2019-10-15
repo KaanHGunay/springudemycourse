8014712cc32b9bb9e66494a7aaea3364f0dd6f37
kaangunay16 <kaangunay16@gmail.com>
2019-10-15 22:37:49 +0300

    Spring Data Init

---
+++ spring-database-udemy/src/main/java/com/khg/springdatabaseudemy/SpringDatabaseUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.khg.springdatabaseudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseUdemyApplication.class, args);
+	}
+
+}
