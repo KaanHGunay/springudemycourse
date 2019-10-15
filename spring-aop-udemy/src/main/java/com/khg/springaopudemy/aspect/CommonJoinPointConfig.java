8867655d419d4d0aedf7d5f37c06ee8e928d9c67
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-15 16:57:06 +0300

    Other @Pointcut Annotation Definitions

--- spring-aop-udemy/src/main/java/com/khg/springaopudemy/aspect/CommonJoinPointConfig.java
+++ spring-aop-udemy/src/main/java/com/khg/springaopudemy/aspect/CommonJoinPointConfig.java
@@ -8,4 +8,13 @@ public class CommonJoinPointConfig {
 
     @Pointcut("execution(* com.khg.springaopudemy.business.*.*(..))")
     public void businessLayerExecution(){}
+
+    @Pointcut("com.khg.springaopudemy.aspect.CommonJoinPointConfig.dataLayerExecution() && com.khg.springaopudemy.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution(){}
+
+    @Pointcut("bean(*dao*)")
+    public void beansContainingDao(){}
+
+    @Pointcut("within(com.khg.springaopudemy.data.*)")
+    public void dataLayerExecutionWithWithin(){}
 }
