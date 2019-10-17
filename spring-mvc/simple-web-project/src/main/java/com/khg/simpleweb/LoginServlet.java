45c3ef1e6fb81f4ebdaf2fe2fdc6288bffb18852
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-17 14:47:20 +0300

    JSP Files

---
+++ spring-mvc/simple-web-project/src/main/java/com/khg/simpleweb/LoginServlet.java
@@ -0,0 +1,17 @@
+package com.khg.simpleweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, res);
+	}
+}
