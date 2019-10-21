f5d871c2a01983d07c3df611d3418e5a2bb888c7
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-21 16:25:31 +0300

    JPQL Basics

---
+++ spring-database-udemy/jpa-hibernate/src/test/java/com/khg/jpahibernate/repository/JPQLTest.java
@@ -0,0 +1,53 @@
+package com.khg.jpahibernate.repository;
+
+import com.khg.jpahibernate.JpaHibernateApplication;
+import com.khg.jpahibernate.entity.Course;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = JpaHibernateApplication.class)
+public class JPQLTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void query_Basic() {
+        Query query = entityManager.createQuery("Select c from Course c");
+        List courses = query.getResultList();
+        logger.info("Select c from Course c -> {}", courses);
+    }
+
+    @Test
+    public void query_WithType() {
+        TypedQuery<Course> query = entityManager.createQuery("Select c from Course c", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info("Select c from Course c -> {}", courses);
+    }
+
+    @Test
+    public void query_WithWhere() {
+        TypedQuery<Course> query =
+                entityManager.createQuery("Select c from Course c where name like 'T%'", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info("Select c from Course c where name like 'T%' -> {}", courses);
+    }
+}
