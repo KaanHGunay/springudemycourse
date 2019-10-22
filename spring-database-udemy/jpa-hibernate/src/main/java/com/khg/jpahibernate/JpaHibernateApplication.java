83b5a5f2972eb7b75e88c1d44b137eb381687a41
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-22 13:37:50 +0300

    JPA Inheritance Hierarchies - Insert/Retrieve All Data

--- spring-database-udemy/jpa-hibernate/src/main/java/com/khg/jpahibernate/JpaHibernateApplication.java
+++ spring-database-udemy/jpa-hibernate/src/main/java/com/khg/jpahibernate/JpaHibernateApplication.java
@@ -1,9 +1,9 @@
 package com.khg.jpahibernate;
 
-import com.khg.jpahibernate.entity.Course;
-import com.khg.jpahibernate.entity.Review;
-import com.khg.jpahibernate.entity.Student;
+import com.khg.jpahibernate.entity.FullTimeEmployee;
+import com.khg.jpahibernate.entity.PartTimeEmployee;
 import com.khg.jpahibernate.repository.CourseRepository;
+import com.khg.jpahibernate.repository.EmployeeRepository;
 import com.khg.jpahibernate.repository.StudentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class JpaHibernateApplication implements CommandLineRunner {
@@ -27,6 +26,9 @@ public class JpaHibernateApplication implements CommandLineRunner {
 	@Autowired
 	private StudentRepository studentRepository;
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHibernateApplication.class, args);
 	}
@@ -34,12 +36,8 @@ public class JpaHibernateApplication implements CommandLineRunner {
 	@Override
 	@Transactional
 	public void run(String... args) throws Exception {
-		Student student = new Student("Kaan");
-		Course course1 = new Course("Spring Boot");
-		Course course2 = new Course("Spring Boot - 2");
-		List<Course> courses = new ArrayList<>();
-		courses.add(course1);
-		courses.add(course2);
-		studentRepository.saveStudentAndCourses(student, courses);
+		employeeRepository.insertEmployee(new PartTimeEmployee("TestPartTimeEmployee", new BigDecimal("50")));
+		employeeRepository.insertEmployee(new FullTimeEmployee("TestFullTimeEmployee", new BigDecimal("10000")));
+		logger.info("All employees -> {}", employeeRepository.retrieveAllEmployee());
 	}
 }
---
+++ spring-database-udemy/jpa-hibernate/src/main/java/com/khg/jpahibernate/repository/EmployeeRepository.java
@@ -0,0 +1,29 @@
+package com.khg.jpahibernate.repository;
+
+import com.khg.jpahibernate.entity.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional
+public class EmployeeRepository {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public void insertEmployee(Employee employee) {
+        entityManager.persist(employee);
+    }
+
+    public List<Employee> retrieveAllEmployee() {
+        return entityManager.createQuery("select e from Employee e", Employee.class).getResultList();
+    }
+}
