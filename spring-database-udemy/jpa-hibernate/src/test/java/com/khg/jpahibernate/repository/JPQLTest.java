e59a9d7a3eb75f8eead35acf7ca8e7a12f3fbec2
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-22 16:12:41 +0300

    JPQL Queries

--- spring-database-udemy/jpa-hibernate/src/test/java/com/khg/jpahibernate/repository/JPQLTest.java
+++ spring-database-udemy/jpa-hibernate/src/test/java/com/khg/jpahibernate/repository/JPQLTest.java
@@ -2,6 +2,7 @@ package com.khg.jpahibernate.repository;
 
 import com.khg.jpahibernate.JpaHibernateApplication;
 import com.khg.jpahibernate.entity.Course;
+import com.khg.jpahibernate.entity.Student;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -49,5 +50,67 @@ public class JPQLTest {
                 entityManager.createNamedQuery("query_get_start_with_t", Course.class);
         List<Course> courses = query.getResultList();
         logger.info("Select c from Course c where name like 'T%' -> {}", courses);
+    }
+
+    @Test
+    public void query_CoursesWithoutStudents() {
+        TypedQuery<Course> query =
+                entityManager.createQuery("select c from Course c where c.students is empty", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info("query_CoursesWithoutStudents -> {}", courses);
+    }
+
+    @Test
+    public void query_CoursesAtLeast2Students() {
+        TypedQuery<Course> query =
+                entityManager.createQuery("select c from Course c where c.students.size >= 1", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info("query_CoursesAtLeast2Students -> {}", courses);
+    }
+
+    @Test
+    public void query_CoursesOrderBy() {
+        TypedQuery<Course> query =
+                entityManager.createQuery("select c from Course c order by c.students.size desc", Course.class);
+        List<Course> courses = query.getResultList();
+        logger.info("query_CoursesOrderBy -> {}", courses);
+    }
+
+    @Test
+    public void query_Like() {
+        TypedQuery<Student> query =
+                entityManager.createQuery("select s from Student s where s.passport.number like '%123%'", Student.class);
+        List<Student> courses = query.getResultList();
+        logger.info("query_Like -> {}", courses);
+    }
+
+    @Test
+    public void join(){
+        Query query = entityManager.createQuery("Select c, s from Course c JOIN c.students s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results Size -> {}", resultList.size());
+        for(Object[] result:resultList){
+            logger.info("Course -> {} Student -> {}", result[0], result[1]);
+        }
+    }
+
+    @Test
+    public void left_join(){
+        Query query = entityManager.createQuery("Select c, s from Course c LEFT JOIN c.students s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results Size -> {}", resultList.size());
+        for(Object[] result:resultList){
+            logger.info("Course -> {} Student -> {}", result[0], result[1]);
+        }
+    }
+
+    @Test
+    public void cross_join(){
+        Query query = entityManager.createQuery("Select c, s from Course c, Student s");
+        List<Object[]> resultList = query.getResultList();
+        logger.info("Results Size -> {}", resultList.size());
+        for(Object[] result:resultList){
+            logger.info("Course -> {} Student -> {}", result[0], result[1]);
+        }
     }
 }
