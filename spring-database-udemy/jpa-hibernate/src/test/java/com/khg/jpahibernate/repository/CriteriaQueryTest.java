3e7b2ff79d1c5f9cd1b5a09046b041ca274aedd6
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-22 17:24:38 +0300

    Criteria Query

--- spring-database-udemy/jpa-hibernate/src/test/java/com/khg/jpahibernate/repository/CriteriaQueryTest.java
+++ spring-database-udemy/jpa-hibernate/src/test/java/com/khg/jpahibernate/repository/CriteriaQueryTest.java
@@ -14,9 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -29,7 +27,7 @@ public class CriteriaQueryTest {
     private EntityManager entityManager;
 
     @Test
-    public void query_Basic() {
+    public void query_SelectAll() {
         // Criteria Builder oluşturulur ve dönüş tipinin hangi class olacağı belirlenir.
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Course> cq = cb.createQuery(Course.class);
@@ -41,4 +39,106 @@ public class CriteriaQueryTest {
         List courses = query.getResultList();
         logger.info("Select c from Course c -> {}", courses);
     }
+
+    @Test
+    public void query_SelectWithCondition() {
+        // Criteria Builder oluşturulur ve dönüş tipinin hangi class olacağı belirlenir.
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // Root Table belirlenir
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // Like tanımlanır
+        Predicate like = cb.like(courseRoot.get("name"), "%T%");
+
+        // Like sorguya eklenir
+        cq.where(like);
+
+        Query query = entityManager.createQuery(cq.select(courseRoot));
+        List courses = query.getResultList();
+        logger.info("Result -> {}", courses);
+    }
+
+    @Test
+    public void all_courses_without_students() {
+        // "Select c From Course c where c.students is empty"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Predicate studentsIsEmpty = cb.isEmpty(courseRoot.get("students"));
+
+        // 4. Add Predicates etc to the Criteria Query
+        cq.where(studentsIsEmpty);
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[Spring in 50 Steps]]
+    }
+
+    @Test
+    public void join() {
+        // "Select c From Course c join c.students s"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Join<Object, Object> join = courseRoot.join("students");
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[JPA in 50 Steps], Course[JPA in 50 Steps], Course[JPA in 50
+        // Steps], Course[Spring Boot in 100 Steps]]
+    }
+
+    @Test
+    public void left_join() {
+        // "Select c From Course c left join c.students s"
+
+        // 1. Use Criteria Builder to create a Criteria Query returning the
+        // expected result object
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // 2. Define roots for tables which are involved in the query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // 3. Define Predicates etc using Criteria Builder
+        Join<Object, Object> join = courseRoot.join("students", JoinType.LEFT);
+
+        // 4. Add Predicates etc to the Criteria Query
+
+        // 5. Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = entityManager.createQuery(cq.select(courseRoot));
+
+        List<Course> resultList = query.getResultList();
+
+        logger.info("Typed Query -> {}", resultList);
+        // [Course[JPA in 50 Steps], Course[JPA in 50 Steps], Course[JPA in 50
+        // Steps], Course[Spring in 50 Steps], Course[Spring Boot in 100 Steps]]
+    }
+
 }
