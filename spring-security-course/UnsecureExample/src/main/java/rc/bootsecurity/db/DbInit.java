f8117fc6b0a3fba4ebff987a501f0c4bf0199f01
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-24 12:59:42 +0300

    Database Authentication

--- spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/db/DbInit.java
+++ spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/db/DbInit.java
@@ -1,5 +1,6 @@
 package rc.bootsecurity.db;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 public class DbInit implements CommandLineRunner {
+    @Autowired
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
@@ -24,9 +26,11 @@ public class DbInit implements CommandLineRunner {
         this.userRepository.deleteAll();
 
         // Crete users
-        User dan = new User("dan",passwordEncoder.encode("dan123"),"USER","");
-        User admin = new User("admin",passwordEncoder.encode("admin123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
-        User manager = new User("manager",passwordEncoder.encode("manager123"),"MANAGER","ACCESS_TEST1");
+        User dan = new User("kaan",passwordEncoder.encode("1"),"USER","");
+        User admin = new User("admin",passwordEncoder.encode("1"),
+                "ADMIN","ACCESS_TEST1, ACCESS_TEST2");
+        User manager = new User("manager",passwordEncoder.encode("1"),
+                "MANAGER","ACCESS_TEST1");
 
         List<User> users = Arrays.asList(dan,admin,manager);
 
