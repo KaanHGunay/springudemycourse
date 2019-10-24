0886993abe93c717fbe1cc6a66905bb4d5159d96
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-24 12:03:02 +0300

    Enable HTTPS/SSL in Spring Boot

--- spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/BootSecurityApplication.java
+++ spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/BootSecurityApplication.java
@@ -17,9 +17,6 @@ public class BootSecurityApplication {
         SpringApplication.run(BootSecurityApplication.class, args);
     }
 
-
-
-/*
     @Bean
     public ServletWebServerFactory servletContainer() {
         // Enable SSL Trafic
@@ -39,21 +36,21 @@ public class BootSecurityApplication {
         tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
 
         return tomcat;
-    }*/
+    }
 
     /*
     We need to redirect from HTTP to HTTPS. Without SSL, this application used
     port 8082. With SSL it will use port 8443. So, any request for 8082 needs to be
     redirected to HTTPS on 8443.
      */
-    /*
+
     private Connector httpToHttpsRedirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8082);
+        connector.setPort(8080);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
-    }*/
+    }
 
 }
