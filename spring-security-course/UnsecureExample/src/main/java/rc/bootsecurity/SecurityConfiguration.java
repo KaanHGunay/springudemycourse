4e4802a31cf033a9d37f71670339c82d5007a821
Kaan Han Günay <kaangunay16@gmail.com>
2019-10-24 10:30:22 +0300

    Configure Role Based Authorization

--- spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ spring-security-course/UnsecureExample/src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -18,14 +18,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()  // Geliştirme için in memory şeklinde çalış
                 .withUser("admin").password(passwordEncoder().encode("123")).roles("ADMIN")  // Admin ekle
                 .and()
-                .withUser("Kaan").password(passwordEncoder().encode("1")).roles("USER");  // Kullanıcı ekle
+                .withUser("Kaan").password(passwordEncoder().encode("1")).roles("USER")  // Kullanıcı ekle
+                .and()
+                .withUser("manager").password(passwordEncoder().encode("1")).roles("MANAGER");
     }
 
+    /**
+     * ant Matcher lar verilen sıra ile çalıştırılır. Bu yüzden birbirini ezmeyecek sırada verilmeli
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/index.html").permitAll()  // Anasayfaya herkes girebilsin
+                .antMatchers("/profile/**").authenticated()  // Herkes kendi profiline girebilsin
+                .antMatchers("/admin/**").hasAnyRole("ADMIN")  // Admin altındaki her şeye erişim sadece admine
+                .antMatchers("/management/index").hasAnyRole("ADMIN", "MANAGER")
                 .and()
                 .httpBasic();
     }
